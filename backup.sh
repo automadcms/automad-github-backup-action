17534672a352d2a89bb28d1637537889c0020907
Marc Anton Dahmen <mail@marcdahmen.de>
2026-06-15 21:09:05 +0200

    feat: make workflow fail on errors in bash script

--- backup.sh
+++ backup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+set -eux
+
 if [ -z ${SSH_HOST} ]; then
 	echo "SSH_HOST is not set. Skipping workflow ..."
-	exit
+	exit 1
 fi
 
-git config --global url.https://x-access-token:${GITHUB_TOKEN}@github.com/.insteadOf https://github.com/
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 if [ -z "${DOCROOT}" ]; then
-	return
+	echo "DOCROOT is not set. Skipping workflow ..."
+	exit 1
 fi
 
 echo '----------------------------------'
@@ -32,15 +34,22 @@ rsync -a --stats \
 	${SSH_USER}@${SSH_HOST}:${DOCROOT}/ .
 
 git add -A
-git commit -m "update content"
+
+if ! git diff --cached --quiet; then
+	git commit -m "update content"
+fi
 
 echo '----------------------------------'
 
 # Create a monthly commit in order to the keep the action alive.
 mkdir -p .github
 date +%m >.github/keep-alive
-git add .
-git commit -m "update keep-alive"
+
+git add -A
+
+if ! git diff --cached --quiet; then
+	git commit -m "update keep-alive"
+fi
 
 echo '----------------------------------'
 
